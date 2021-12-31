bb389c6befd9197c203d3b5ee8ac49b427853e5d
zinx110 <tasdid110@gmail.com>
2022-01-01 02:32:40 +0600

    bug fix again , i cry

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -21,7 +21,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         <p className="truncate max-w-md "> {result.overview}</p>
 
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold ">
-          {result?.title || result?.original.name}
+          {result?.title || result?.original_name}
         </h2>
 
         <p className="flex items-center opacity-100 sm:opacity-0 sm:group-hover:opacity-100 ">
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import requests from "../utils/requests";
 import axios from "axios";
 
 export default function Home({ results }) {
-  console.log(results);
   return (
     <div>
       <Head>
